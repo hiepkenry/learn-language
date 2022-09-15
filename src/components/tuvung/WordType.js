b9f4e0dd974d7956ab632902aeb930e79a8d9412
canvanhiep <hiep@hol-on.co.jp>
2022-09-15 17:48:50 +0900

    update word

--- src/components/tuvung/WordType.js
+++ src/components/tuvung/WordType.js
@@ -1,25 +1,52 @@
 import { useEffect } from 'react';
-import Jlpt from './Jlpt';
+import Job from '../Job';
 import Wrapper from '../../assets/wrappers/JobsContainer';
-// import { useSelector, useDispatch } from 'react-redux';
-// import Loading from './Loading';
-// import { getAllJobs } from '../features/allJobs/allJobsSlice';
-// import PageBtnContainer from './PageBtnContainer';
-const WordType = () => {
+import { useSelector, useDispatch } from 'react-redux';
+import Loading from '../Loading';
+import { getAllJobs } from '../../features/allJobs/allJobsSlice';
+import PageBtnContainer from '../PageBtnContainer';
+const JobsContainer = () => {
+  const {
+    jobs,
+    isLoading,
+    page,
+    totalJobs,
+    numOfPages,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+  } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
 
-  const jpltContent = [{ "title": "Mimikara", "_id": 1, "level": 2 }, { "title": "3000 từ vựng", "_id": 2, "level": 2 }, { "title": "kikutan", "_id": 3, "level": 2 }, { "title": "JLPT test", "_id": 4, "level": 2 }];
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, [page, search, searchStatus, searchType, sort]);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
 
   return (
     <Wrapper>
-      {/* <h5>{totalJobs} job{jobs.length > 1 && 's'} found</h5> */}
+      <h5>{totalJobs} job{jobs.length > 1 && 's'} found</h5>
       <div className='jobs'>
-        {jpltContent.map((jlpt) => {
-          return <Jlpt key={jlpt._id} {...jlpt} />;
+        {jobs.map((job) => {
+          // console.log(job);
+          return <Job key={job._id} {...job} />;
         })}
       </div>
-      {/* {numOfPages > 1 && <PageBtnContainer />} */}
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
 
-export default WordType;
+export default JobsContainer;
