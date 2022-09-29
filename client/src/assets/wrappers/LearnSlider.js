355c68227dd0ceded3b83214a4ea10ac34b5ab1a
canvanhiep <hiep@hol-on.co.jp>
2022-09-29 17:15:34 +0900

    update slider

---
+++ client/src/assets/wrappers/LearnSlider.js
@@ -0,0 +1,126 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.article`
+  background: var(--white);
+  border-radius: var(--borderRadius);
+  display: grid;
+  grid-template-rows: 1fr auto;
+  box-shadow: var(--shadow-2);
+
+  header {
+     padding: 2rem 1.5rem;
+    border-bottom: 1px solid var(--grey-100);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    h5 {
+      letter-spacing: 0;
+    }
+  }
+  .main-icon {
+    width: 60px;
+    height: 60px;
+    display: grid;
+    place-items: center;
+    background: var(--primary-500);
+    border-radius: var(--borderRadius);
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: var(--white);
+    margin-right: 2rem;
+  }
+  .info {
+    h5 {
+      margin-bottom: 0.25rem;
+      color: var(--primary-600);
+      font-size: 20px;
+      font-weight: bold;
+      column-gap: 1rem;
+       display: flex;
+
+    }
+    p {
+      margin: 0;
+      text-transform: capitalize;
+      color: var(--black);
+      /* letter-spacing: var(--letterSpacing); */
+    }
+    .hantu{
+       color: var(--grey-500);
+      /* letter-spacing: var(--letterSpacing); */
+      font-size: small;
+    }
+  }
+  img{
+    width:150px;
+    height:120px;
+  }
+ .speechMenu{
+        font-size: 19px;
+         align-self: center;
+        color: var(--primary-500);
+        /* height: 20px; */
+       }
+  .public {
+    background: #e0e8f9;
+    color: #647acb;
+  }
+  .private {
+    color: #d66a6a;
+    background: #ffeeee;
+  }
+  .content {
+    padding: 0.5rem 1rem;
+    .jptext {
+    display: flex;
+    column-gap: 1rem;
+    }
+  }
+  .text {
+    text-transform: capitalize;
+    /* letter-spacing: var(--letterSpacing); */
+  }
+  .content-center {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 0.5rem;
+    column-gap: 1rem;
+    @media (min-width: 576px) {
+      grid-template-columns: 1fr 2fr;
+    }
+  }
+
+  .status {
+    border-radius: var(--borderRadius);
+    text-transform: capitalize;
+    /* letter-spacing: var(--letterSpacing); */
+    text-align: center;
+    width: 100px;
+    height: 30px;
+    margin-top: 0.5rem;
+  }
+  footer {
+    margin-top: 1rem;
+  }
+  .edit-btn,
+  .delete-btn {
+    /* letter-spacing: var(--letterSpacing); */
+    cursor: pointer;
+    height: 30px;
+  }
+  .edit-btn {
+    color: var(--green-dark);
+    background: var(--green-light);
+    margin-right: 0.5rem;
+  }
+  .delete-btn {
+    color: var(--red-dark);
+    background: var(--red-light);
+  }
+  &:hover .actions {
+    visibility: visible;
+  }
+`;
+
+export default Wrapper;
--- client/src/components/Learning/ShowText.js
+++ client/src/components/Learning/ShowText.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-
-import Wrapper from '../../assets/wrappers/JobsContainer';
+import { Navigate } from 'react-router-dom';
+import Wrapper from '../../assets/wrappers/LearnSlider';
 import { useSelector, useDispatch } from 'react-redux';
 import Loading from '../Loading';
 import { getAllVocabs } from '../../features/allVocabs/allVocabsSlice';
 import PageBtnContainer from '../PageBtnContainer';
-import TransText from './TransText';
+// import TransText from './TransText';
 import TextTransition, { presets } from "react-text-transition";
 const ShowText = () => {
   const {
@@ -13,13 +13,7 @@ const ShowText = () => {
     isLoading,
     page,
     totalVocabs,
-    numOfPages,
-    search,
-    searchStatus,
-    searchType,
-    searchTitle,
-    searchCurriculum,
-    sort,
+
   } = useSelector((store) => store.allVocabs);
   const dispatch = useDispatch();
 
@@ -32,49 +26,85 @@ const ShowText = () => {
     return () => clearTimeout(intervalId);
   }, []);
   if (isLoading) {
-    return <Loading center />;
+    return <Navigate to='/tu-vung' />;
   }
-  if (vocabs.length === 0) {
-    return (
-      <Wrapper>
-        <h2>No data to display...</h2>
+  // if (vocabs.length === 0) {
+  //   return (
+  //     <Wrapper>
+  //       <h2>No data to display...</h2>
 
-      </Wrapper>
-    );
-  }
+  //     </Wrapper>
+  //   );
+  // }
 
 
   const kanji = vocabs.map((itemX) => {
     return itemX.kanji
   })
+  const hantu = vocabs.map((itemX) => {
+    return itemX.hantu
+  })
   const hiragana = vocabs.map((itemX) => {
     return itemX.hiragana
+  })
+  const vdjp = vocabs.map((itemX) => {
+    return itemX.vdjp
+  })
+  const vdvn = vocabs.map((itemX) => {
+    return itemX.vdvn
   })
   const tiengviet = vocabs.map((itemX) => {
     return itemX.vn
   })
+  const imageUrl = vocabs.map((itemX) => {
+    return itemX.image
+  })
 
   return (
     <Wrapper>
-      <h5>
+      {/* <h5>
         {totalVocabs} vocab{vocabs.length > 1 && 's'} found
-      </h5>
-      <div className='jobs'>
+      </h5> */}
+      <header>
+        <div className='info'>
+          <div className='content-center'>
+            {/* <TransText key={1} {...vocabs} /> */}
+            {/* <h1> */}
+            <div className="content-left">
+              <TextTransition springConfig={presets.slow}>
+                <h4>{kanji[index % kanji.length]}</h4>
+              </TextTransition>
+              <TextTransition springConfig={presets.slow}>
+                <p className='hantu'>{hantu[index % hantu.length]}</p>
+              </TextTransition>
+              <TextTransition springConfig={presets.slow}>
+                <p>{hiragana[index % hiragana.length]}</p>
+              </TextTransition>
+              <TextTransition springConfig={presets.slow}>
+                <p>{tiengviet[index % tiengviet.length]}</p>
+              </TextTransition>
+            </div>
+            <div className="content-right">
+              <TextTransition springConfig={presets.slow}>
+                <img src={process.env.PUBLIC_URL + "images/" + imageUrl[index % imageUrl.length]} />
+              </TextTransition>
+              <TextTransition springConfig={presets.slow}>
+                <p>{vdjp[index % vdjp.length]}</p>
+              </TextTransition>
+              <TextTransition springConfig={presets.slow}>
+                <p>{vdvn[index % vdvn.length]}</p>
+              </TextTransition>
 
-        {/* <TransText key={1} {...vocabs} /> */}
-        <h1>
-          <TextTransition springConfig={presets.slow}>
-            <h2>{kanji[index % kanji.length]}</h2>
-          </TextTransition>
-          <TextTransition springConfig={presets.slow}>
-            <h3>{hiragana[index % hiragana.length]}</h3>
-          </TextTransition>
-          <TextTransition springConfig={presets.slow}>
-            <h3>{tiengviet[index % tiengviet.length]}</h3>
-          </TextTransition>
-        </h1>
-      </div>
-      {/* {numOfPages > 1 && <PageBtnContainer />} */}
+            </div>
+
+
+
+
+            {/* </h1> */}
+          </div>
+        </div>
+        {/* {numOfPages > 1 && <PageBtnContainer />} */}
+      </header>
     </Wrapper>
   );
 };
