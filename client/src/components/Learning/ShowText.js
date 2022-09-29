1e041f4de45356da029d027416906db49015cbb9
canvanhiep <hiep@hol-on.co.jp>
2022-09-29 18:03:00 +0900

    update slider

--- client/src/components/Learning/ShowText.js
+++ client/src/components/Learning/ShowText.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import Wrapper from '../../assets/wrappers/LearnSlider';
 import { useSelector, useDispatch } from 'react-redux';
+import { useSpeechSynthesis } from "react-speech-kit";
 import Loading from '../Loading';
 import { getAllVocabs } from '../../features/allVocabs/allVocabsSlice';
 import PageBtnContainer from '../PageBtnContainer';
@@ -16,15 +17,31 @@ const ShowText = () => {
 
   } = useSelector((store) => store.allVocabs);
   const dispatch = useDispatch();
-
+  const [highlightedText, setHighlightedText] = useState("")
+  const [highlightedVDText, setHighlightedVDText] = useState("")
+  const [highlightedEng, sethighlightedEng] = useState("")
   const [index, setIndex] = useState(0);
+  const [textIndex, settextIndex] = useState(0);
+  const [pitch, setPitch] = useState(1);
+  const [rate, setRate] = useState(1);
+  const onEnd = () => {
+    setHighlightedText(hiragana[textIndex])
+    setHighlightedVDText(vdjp[textIndex])
+    // sethighlightedEng(en[index])
+    setTimeout(() => {
+      setIndex(index => index + 1)
+    }, 3000);
+    setTimeout(() => {
+      settextIndex(textIndex => textIndex + 1)
+    }, 3000);
+
+
+  }
+  const { cancel, supported, speak, voices } = useSpeechSynthesis({ onEnd })
+
   useEffect(() => {
-    const intervalId = setInterval(() =>
-      setIndex(index => index + 1),
-      5000 // every 3 seconds
-    );
-    return () => clearTimeout(intervalId);
-  }, []);
+    speak({ text: highlightedText, voices, rate, pitch })
+  }, [textIndex]);
   if (isLoading) {
     return <Navigate to='/tu-vung' />;
   }
@@ -59,7 +76,13 @@ const ShowText = () => {
   const imageUrl = vocabs.map((itemX) => {
     return itemX.image
   })
-
+  const startSpeed = () => {
+    setHighlightedText(hiragana[textIndex])
+    setHighlightedVDText(vdjp[textIndex])
+    setIndex(0)
+    settextIndex(0)
+    speak({ text: highlightedText, voices, rate, pitch })
+  }
   return (
     <Wrapper>
       {/* <h5>
@@ -71,27 +94,27 @@ const ShowText = () => {
             {/* <TransText key={1} {...vocabs} /> */}
             {/* <h1> */}
             <div className="content-left">
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <h4>{kanji[index % kanji.length]}</h4>
               </TextTransition>
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <p className='hantu'>{hantu[index % hantu.length]}</p>
               </TextTransition>
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <p>{hiragana[index % hiragana.length]}</p>
               </TextTransition>
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <p>{tiengviet[index % tiengviet.length]}</p>
               </TextTransition>
             </div>
             <div className="content-right">
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <img src={process.env.PUBLIC_URL + "images/" + imageUrl[index % imageUrl.length]} />
               </TextTransition>
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <p>{vdjp[index % vdjp.length]}</p>
               </TextTransition>
-              <TextTransition springConfig={presets.slow}>
+              <TextTransition springConfig={presets.slow} delay={6000}>
                 <p>{vdvn[index % vdvn.length]}</p>
               </TextTransition>
 
@@ -104,6 +127,13 @@ const ShowText = () => {
           </div>
         </div>
         {/* {numOfPages > 1 && <PageBtnContainer />} */}
+        <button
+          type='button'
+          className='btn delete-btn'
+          onClick={() => startSpeed()}
+        >
+          Start
+        </button>
       </header>
     </Wrapper>
   );
